9a7c6e9dcd973fffd628e8fceeccf75be814d0c9
artephakt-studio <artephakt.studio@gmail.com>
2021-03-14 13:33:42 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vue-c2-l19-1-5d1lsn
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vue-c2-l19-1-5d1lsn)
---
+++ index.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import './style.css';
+
+new Vue({
+  el: '#app',
+  data: {
+    fruits: ['banane','orange','kiwi']
+  }
+});
